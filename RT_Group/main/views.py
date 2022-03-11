1dc9463d51742f3f54ac163bec4c494e91d4eb9a
DiForzza <uit@pm.convex.ru>
2022-03-11 13:02:30 +0500

    weather and format text

--- RT_Group/main/views.py
+++ RT_Group/main/views.py
@@ -4,7 +4,7 @@ import requests
 
 def index(request):
     url = 'http://wttr.in/Ekaterinburg?0T'
-    response = requests.get(url)
+    response = requests.get(url, params={'format': 2, 'M': ''})
     return render(request, 'main/main.html', {'response': response.text})
 
 
