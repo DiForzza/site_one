f30da4000b6a22666698680a78acfb617a58d9eb
DiForzza <uit@pm.convex.ru>
2022-03-21 13:00:34 +0500

    {% if current_url and 'home' or 'about' %}

--- RT_Group/main/views.py
+++ RT_Group/main/views.py
@@ -31,19 +31,6 @@ def about(request):
 
 def authorization(request):
     error = ''
-    # if request.method == 'POST':
-    #     form = UserLoginForm(request.POST or None)
-    #     if form.is_valid():
-    #         username = User.objects.get(email=form.cleaned_data['email'])
-    #         password = form.cleaned_data['password']
-    #         user = authenticate(username=username, password=password)
-    #         if user:
-    #             if user.is_active:
-    #                 auth_login(request, user)
-    #                 return HttpResponseRedirect(request.GET.get('next',
-    #                                                             settings.LOGIN_REDIRECT_URL))
-    #         else:
-    #             error = 'Invalid username or password.'
     context = {
         'response': weather(),
         'error': error
