d105bf209aed8593e190844a2198039fe1316702
BuildTools <unconfigured@null.spigotmc.org>
2022-03-21 21:22:56 +0500

    added timestamp

--- RT_Group/main/models.py
+++ RT_Group/main/models.py
@@ -1,9 +1,11 @@
 from django.db import models
+import datetime
 
 
 class Task(models.Model):
     title = models.CharField("Название", max_length=50)
     task = models.TextField("Описание")
+    date = models.DateTimeField(u'Дата и время', default=datetime.datetime.now())
 
     def __str__(self):
         return self.title
