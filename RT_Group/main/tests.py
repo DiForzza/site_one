83205e453495d7ff9e5c2aea456bf1f3f97b6b2a
DiForzza <uit@pm.convex.ru>
2022-03-29 13:01:28 +0500

    third test

--- RT_Group/main/tests.py
+++ RT_Group/main/tests.py
@@ -1,5 +1,21 @@
-from django.test import TestCase
+from itertools import product, permutations
 
-nums = [55, 44, 33, 22, 11]
-if all([i < 56 for i in nums]):
-    print('all larger')
+
+def factorial(x):
+    print('start x', x)
+    z = x
+
+    def result(z):
+        if z == 0:
+            print(z, 'if')
+        else:
+            print((factorial(x) + factorial(x)), 'else', x)
+            z -= z
+
+    result(z)
+
+def is_odd(x):
+    print(factorial(x), x)
+
+
+factorial(6)
