621fb14027f874607742ce6e0a80ae71c461b8d2
DiForzza <uit@pm.convex.ru>
2022-04-08 13:00:05 +0500

    worked times

--- RT_Group/RT_Group/asgi.py
+++ RT_Group/RT_Group/asgi.py
@@ -1,28 +1,16 @@
-# import os
-#
-# from django.core.asgi import get_asgi_application
-#
-# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'RT_Group.settings')
-#
-# application = get_asgi_application()
-
-
-# Импортировать модуль os
 import os
-# Импортировать get_asgi_application для обработки протокола http
 from django.core.asgi import get_asgi_application
-# Import ProtocolTypeRouter и URLRouter для установки маршрутизации веб-сокетов
 from channels.routing import ProtocolTypeRouter, URLRouter
-# Import AuthMiddlewareStack для обработки веб-сокета
 from channels.auth import AuthMiddlewareStack
-# Импортировать маршрутизацию веб-сокетов
 from main.routing import ws_urlpatterns
 
-# Назначьте значение для DJANGO_SETTINGS_MODULE
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'RT_Group.settings')
 
-# Определить переменные приложения для обработки HTTP и WebSocket
 application = ProtocolTypeRouter({
     'http': get_asgi_application(),
-    'websocket': AuthMiddlewareStack(URLRouter(ws_urlpatterns))
+    'websocket': AuthMiddlewareStack(
+        URLRouter(
+            ws_urlpatterns
+        )
+    )
 })
--- RT_Group/main/consumers.py
+++ RT_Group/main/consumers.py
@@ -1,25 +1,40 @@
-# Импортировать модуль JSON
 import json
-# Импортировать WebsocketConsumer
 from channels.generic.websocket import WebsocketConsumer
-# Импортировать модуль datetime
 from datetime import datetime
-# Импортировать модуль sleep
 from time import sleep
+import time
 from channels.exceptions import StopConsumer
 
 
-# Определить класс потребителя для отправки данных через
 class ws_consumer(WebsocketConsumer):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(args, kwargs)
+        self._start_time = time.perf_counter()
 
     def connect(self):
         self.accept()
-        while True:
-            now = datetime.now()
-            self.send(json.dumps({'timeValue': now.strftime("%H:%M:%S")}))
-            sleep(1)
+        self.timer()
 
-    def disconnect(self, timer_run):
+    def disconnect(self, close_code):
         print('!!!Disconnect!!!')
         self.close()
         raise StopConsumer()
+
+    def render(self):
+        print('!!!render!!!')
+
+    def receive(self, text_data=None, bytes_data=None):
+        print('!!!receive!!!')
+
+    def timer(self):
+        start_time = True
+        while start_time:
+            now = datetime.now()
+            self.send(json.dumps({'timeValue': now.strftime("%H:%M:%S")}))
+            sleep(1)
+            elapsed_time = time.perf_counter() - self._start_time
+            print(elapsed_time)
+            if elapsed_time > 20:
+                start_time = False
+                print('timer stopped')
